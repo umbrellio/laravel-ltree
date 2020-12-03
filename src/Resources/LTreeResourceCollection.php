7516f57218549c5c7edc55a0eabe64d754926163
Korben Dallas <pvsaintpe@icloud.com>
2020-12-04 00:28:20 +0300

    linter fixes

--- src/Resources/LTreeResourceCollection.php
+++ src/Resources/LTreeResourceCollection.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Umbrellio\LTree\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Collection;
 use Umbrellio\LTree\Collections\LTreeCollection;
 
 abstract class LTreeResourceCollection extends ResourceCollection
 {
-    public function __construct(LTreeCollection $resource, $sort = null, bool $usingSort = true)
+    /**
+     * @param LTreeCollection|Collection $resource
+     */
+    public function __construct($resource, $sort = null, bool $usingSort = true)
     {
         $collection = $resource->toTree($usingSort);
 
